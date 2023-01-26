ef27e98be8bc18e0b1e4101c6e18f81166f09abc
moin59 <eee.moin@gmail.com>
2023-01-26 21:48:08 +0100

    add service

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { TASKS } from 'src/app/mock-tasks';
+import { Task } from 'src/app/Task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  getTask(): Observable<Task[]> {
+
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
