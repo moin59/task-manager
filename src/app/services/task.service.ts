3d6acdaebb3e6a9e2e825266f284d4c1e4e7ac27
moin59 <eee.moin@gmail.com>
2023-01-27 19:59:16 +0100

    add http

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
@@ -7,6 +8,8 @@ import { Task } from 'src/app/Task';
   providedIn: 'root'
 })
 export class TaskService {
+
+  private apiUrl = 'http://localhost:3000/tasks';
 
   constructor() { }
 
